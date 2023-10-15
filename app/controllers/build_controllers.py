3fac05cd3b58a2168761fa9ebf20e23e2cb97bdb
vuluu2k <vuluu040320@gmail.com>
2023-10-15 22:47:24 +0700

    limit token

--- app/controllers/build_controllers.py
+++ app/controllers/build_controllers.py
@@ -1,7 +1,21 @@
 from fastapi.encoders import jsonable_encoder
+import tiktoken
 from app.controllers.key_controllers import get_openai_key_by_user_id
 import openai
 
+
+def nums_token_from_message(messages, model="gpt-3.5-turbo-0301"):
+    encoding = tiktoken.encoding_for_model(model)
+    nums_token = 0
+    for message in messages:
+        nums_token += 4
+        for key, value in message.items():
+            nums_token += len(encoding.encode(value))
+            if key == "name":
+                nums_token += -1
+    
+    nums_token += 2
+    return nums_token
 
 async def build_learning_path(request, body):
     openai_key = await get_openai_key_by_user_id(request, body.user_id)
