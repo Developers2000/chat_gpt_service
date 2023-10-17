f1f6da7ea35b8499356628f2744a6877ff6ca599
vuluu <vuluu040320@gmail.com>
2023-10-17 14:16:55 +0700

    test

--- app/controllers/build_controllers.py
+++ app/controllers/build_controllers.py
@@ -7,17 +7,26 @@ from app.controllers.key_controllers import get_openai_key_by_user_id
 # openai.api_base = "http://localhost:1337"
 
 
-import g4f, asyncio
+import g4f
+import asyncio
 
 _providers = [
     g4f.Provider.Aichat,
     g4f.Provider.ChatBase,
     g4f.Provider.Bing,
     g4f.Provider.GptGo,
-    # g4f.Provider.You,
-    # g4f.Provider.Yqcloud,
+    g4f.Provider.You,
+    g4f.Provider.Yqcloud,
+    g4f.Provider.Liaobots,
 ]
- 
+
+_providers_sync = [
+    g4f.Provider.Raycast,
+    g4f.Provider.GeekGpt,
+    g4f.Provider.Theb,
+    g4f.Provider.Vercel,
+]
+
 
 async def run_provider(provider: g4f.Provider.BaseProvider, message: str = "Hello"):
     try:
@@ -29,8 +38,13 @@ async def run_provider(provider: g4f.Provider.BaseProvider, message: str = "Hell
 
         return {f"{provider.__name__}": response}
     except Exception as e:
-        return {f"{provider.__name__}": e}
-        
+        return {f"{provider.__name__}": str(e)}
+
+
+async def run_provider_sync(provider: g4f.Provider.BaseProvider, message: str = "Hello"):
+    pass
+
+
 async def run_all(message: str = "Hello"):
     calls = [
         run_provider(provider, message) for provider in _providers
@@ -38,9 +52,7 @@ async def run_all(message: str = "Hello"):
     return await asyncio.gather(*calls)
 
 
-
 async def build_learning_path(request, body):
     responses = await run_all(body.message)
 
-
-    return {"responses" : responses}
+    return {"responses": responses}
